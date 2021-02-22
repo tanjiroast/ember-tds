046695302e75bd182c46d8dd54abef1444b6133a
Gaëlle <21902643@etu.unicaen.fr>
2021-02-22 16:00:23 +0100

    afterModel + last commit

--- app/routes/categories.js
+++ app/routes/categories.js
@@ -5,4 +5,10 @@ export default class CategoriesRoute extends Route {
   model() {
     return new MyCategories(this.store.findAll('category'));
   }
+  afterModel(model) {
+    this.store.findAll('contact');
+    if (model.length > 0) {
+      this.transitionTo(`categories.contacts`, model.get('firstObject').id);
+    }
+  }
 }
